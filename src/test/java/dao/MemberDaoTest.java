5f64e97f96b97eaff4eb9c194fe42e1076d33d14
qhfudwl <qhfudwl@naver.com>
2022-01-06 19:17:46 +0900

    mysql test1

---
+++ src/test/java/dao/MemberDaoTest.java
@@ -0,0 +1,9 @@
+package dao;
+
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+
+//@SpringJUnitConfig(classes= {DataSourceConfig.class})
+public class MemberDaoTest {
+
+}
