1ef6017ae5fff00124e132079c2aa8af3e1c3e3a
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-10 18:22:45 +0900

    update chart

--- src/main/webapp/resources/js/incl/chart.js
+++ src/main/webapp/resources/js/incl/chart.js
@@ -35,12 +35,12 @@ function drawAgeChartWrap() {
 	function drawAgeChart() {
 		var data = google.visualization.arrayToDataTable([
 			['Element', 'Age', { role: 'style' }],
-			['10대', 10, 'color:' + color[0]],
-			['20대', 25, 'color:' + color[1]],
-			['30대', 18, 'color:' + color[2]],
-			['40대', 5, 'color:' + color[3]],
-			['50대', 3, 'color:' + color[4]],
-			['60대', 1, 'color:' + color[5]]
+			['10대', ages[0], 'color:' + color[0]],
+			['20대', ages[1], 'color:' + color[1]],
+			['30대', ages[2], 'color:' + color[2]],
+			['40대', ages[3], 'color:' + color[3]],
+			['50대', ages[4], 'color:' + color[4]],
+			['60대', ages[5], 'color:' + color[5]]
 		]);
 
 		var options = {
@@ -68,7 +68,7 @@ let femaleCount, maleCount;
 let ages = [];
 let userCount;
 let bathroom, specialDay, clean, parkinig, goodgroup, alone, big, interior;
-let color = [];
+let color = new Array(6);
 $(document).ready(function() {
 
 	/*
@@ -92,6 +92,7 @@ $(document).ready(function() {
 			for(let i in data["restReference"].ageCount){
 				ages[i] = data["restReference"].ageCount[i];
 			}
+			console.log("ages[0] is"+ages[0]);
 			
 			//리뷰 데이터 받기
 			/* 추후 변경 가능성 있음 */
@@ -120,24 +121,18 @@ $(document).ready(function() {
 		
 	*/
 	$.when(promise).done(function(data) {
-
-		/*
-			성별 차트그리기
-		*/
+		//성별 차트그리기
 		drawGenderChartWrap()
 		
-		/*
-			연령별 차트그리기
-		*/
-		
+		//연령별 차트그리기
 		//1.컬러초기화
 		setAgeChartColor()
 		
-		
 		//2.1등 컬러 셋팅하기
+		setBestAgeChartColor()
 		
-		
-		//3.치ㅏ
+		//3.연령별 차트 그리기
+		drawAgeChartWrap()
 	});
 
 
@@ -146,7 +141,8 @@ $(document).ready(function() {
 
 //연령별 그래프 컬러 초기화
 function setAgeChartColor(){
-	for(let i in color){
+	//향상된 for문 안됨 왜?? 추측1) 배열이 빈값이라서 (사이즈와상관없이)
+	for(let i=0;i<color.length;i++){
 		color[i] = "#c2c9cd";
 	}
 }
@@ -158,12 +154,27 @@ function setAgeChartColor(){
 */
 function setBestAgeChartColor(){
 	//1. 배열내에 최대값 찾기
-	let maxNum = Math.max.apply(null, color);
-	console.log("maxNum is "+maxNum);
+	let maxNum = Math.max.apply(null, ages);
+
 	//2. 최대값 중복검사
+	//2-1. 최대값이 포함되어 있는 배열의 index값 리턴
+	let maxNumIndex = ages.indexOf(maxNum);
+
+	//2-2. 배열 복사후 배열에서 최대값을 빼준다
+	let newAges = ages.filter(()=>true);
+	newAges.splice(maxNumIndex,1);
+
+	//2-3. 최대값을 빼준 배열에서 똑같은 값이 있다면, 최대값이 중복이라 색깔 지정 x,
+	//없으면 최대값은 유일한 값을 가짐으로 색깔을 준다.
+	if(newAges.includes(maxNum)){
+		return;
+	}
+	else{
+		//2-4. 연령별 최대값을 가지는 1개 열에만 컬러를 준다
+		color[maxNumIndex]= "#508fe8";
+	}
 	
-	
-	
+
 }
 
 
