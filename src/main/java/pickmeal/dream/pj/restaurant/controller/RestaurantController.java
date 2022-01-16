8be21159adcf916b7fdba5c8228af2a934f26882
yoon <yoon@localhost>
2022-01-16 19:36:53 +0900

    merge index

--- src/main/java/pickmeal/dream/pj/restaurant/controller/RestaurantController.java
+++ src/main/java/pickmeal/dream/pj/restaurant/controller/RestaurantController.java
@@ -30,6 +30,13 @@ public class RestaurantController {
 	@Autowired
 	MessageService ms;
 	
+	/**
+	 * @author 김보령
+	 */
+	static final double EARTH = 6371000; // 단위 m
+	static final double RADIUS = (Math.PI / 180);
+	
+	
 	/*
 	 *  메인화면 불러오기
 	 * 
@@ -38,17 +45,16 @@ public class RestaurantController {
 	public ModelAndView index() {
 		
 		
-		//레스토랑 아이디 셋팅
+		//레스토랑 아이디 셋팅(임시)- 윤효심
 		long restaurantId =1;
 		
-		//포춘쿠키 메세지 셋팅
-		
+		//포춘쿠키 메세지 셋팅 - 윤효심
 		String fortuneMessage = ms.getMessageByType('F');
 		
 		ModelAndView mav = new ModelAndView();
-		//레스토랑 아이디 추가
+		//레스토랑 아이디 추가 - 윤효심 
 		mav.addObject("restaurantId",restaurantId);
-		//포춘메세지 추가
+		//포춘메세지 추가 - 윤효심 
 		mav.addObject("fortuneMessage",fortuneMessage);
 		//View 이름 설정
 		mav.setViewName("index");
@@ -61,21 +67,51 @@ public class RestaurantController {
 	/**
 	 * 식당 리뷰 관련 정보 
 	 * 		- 식당 성별/연령별 정보
+	 * 
+	 * @param restaurantId
+	 * @return
+	 * @author 윤효심
+	 */
+	@PostMapping("restaurant_sub_info_reference")
+	@ResponseBody
+	public HashMap<String, Object> getRestaurantSubInfoForReference(@RequestParam long restaurantId){
+		//레스토랑 정보가 있으면 
+		RestaurantReference restReference = null;
+		
+		
+		restReference = rrs.getRestaurantReference(restaurantId);
+
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		
+		//레스토랑 정보가 없으면
+		result.put("restReference", restReference);
+		
+		
+		return result;
+		
+	}
+	
+	/**
+	 * 식당 리뷰 관련 정보 
 	 * 		- 식당 리뷰들 정보
 	 * 
 	 * @param restaurantId
 	 * @return
 	 * @author 윤효심
 	 */
-	@PostMapping("restaurant_sub_info")
+	@PostMapping("restaurant_sub_info_review")
 	@ResponseBody
-	public HashMap<String, Object> getRestaurantSubInfo(@RequestParam long restaurantId){
-
-		RestaurantReference restReference = rrs.getRestaurantReference(restaurantId);
-		Review review = rs.getReview(restaurantId);
+	public HashMap<String, Object> getRestaurantSubInfoForReview(@RequestParam long restaurantId){
+		//레스토랑 정보가 있으면 
+		
+		Review review = null;
+		
+		
+		review = rs.getReview(restaurantId);
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		
-		result.put("restReference", restReference);
+		//레스토랑 정보가 없으면
+	
 		result.put("review", review);
 		
 		return result;
@@ -83,11 +119,57 @@ public class RestaurantController {
 	}
 	
 	
-	
-	@GetMapping("usePointPopup")
-	public String testUserPoint() {
-		return "/index2";
+	/**
+	 * 
+	 * @param nowLat
+	 * @param nowLng
+	 * @param diffM
+	 * @return
+	 * @author 김보령
+	 */
+	@GetMapping("/calculateMap")
+	@ResponseBody
+	public ResponseEntity<?> calculateMap(@RequestParam("nowLat") double nowLat, 
+			@RequestParam("nowLng") double nowLng, @RequestParam("diffM") double diffM) {
+		
+
+		double diffLat = LatitudeInDifference(diffM);
+		double diffLng = LongitudeInDifference(nowLat, diffM);
+		
+		double lat1 = nowLat - diffLat;
+		double lng1 = nowLng - diffLng;
+		double lat2 = nowLat + diffLat;
+		double lng2 = nowLng + diffLng;
+		
+		double[] latLng = {lat1, lng1, lat2, lng2};
+		
+		return ResponseEntity.ok(latLng);
 	}
+	
+	/**
+	 * 
+	 * @param diff
+	 * @return
+	 * @author 김보령 
+	 */
+    //반경 m이내의 위도차(degree)
+    public double LatitudeInDifference(double diff){
+ 
+        return (diff*360.0) / (2*Math.PI*EARTH);
+    }
+ 
+    /**
+     * 
+     * @param _latitude
+     * @param diff
+     * @return
+     * @author 김보
+     */
+    //반경 m이내의 경도차(degree)
+    public double LongitudeInDifference(double _latitude, double diff){
+ 
+        return (diff*360.0) / (2*Math.PI*EARTH*Math.cos(Math.toRadians(_latitude)));
+    }
 	
 
 	
--- src/main/webapp/WEB-INF/views/posting/index.jsp
+++ src/main/webapp/WEB-INF/views/posting/index.jsp
@@ -6,24 +6,18 @@
 <head>
 <meta charset="UTF-8">
 <%@ include file="/WEB-INF/views/incl/link.jsp"%>
-<!--  그래프 자료 - 윤효심-->
+<!--  그래프 자료 -->
 <script type="text/javascript"
 	src="https://www.gstatic.com/charts/loader.js"></script>
-<!--  가게 부가 정보 - 윤효심-->
+<!--  가게 부가 정보 -->
 <link
 	href="${pageContext.request.contextPath}/resources/css/incl/chart.css"
 	rel="stylesheet" type="text/css">
 <script
 	src="${pageContext.request.contextPath}/resources/js/incl/chart.js"
 	defer></script>
-<!--  포춘쿠키 - 윤효심-->
+<!--  포춘쿠키 -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
-<!-- 김보령  -->
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=001358587c4d106ce5a3702588b8ce85&libraries=services"></script>
-<script src="${pageContext.request.contextPath}/resources/js/incl/index_map.js" defer></script>
-<link  rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/incl/index_map_b.css" />
-
-
 
 <title>밥찡코</title>
 
@@ -31,33 +25,9 @@
 </head>
 <body>
 	<%@ include file="/WEB-INF/views/incl/header.jsp"%>
-	<!-- 김보령 -->
-	<section id="content">
-	<h2 class="hidden">게임하기</h2>
-	<section id="mapWrap">
-		<h3 class="hidden">지도 표시</h3>
-		<div id="mapWindow">
-			<div id="buttonWrap">
-				<button name="radius" class="radius" value="300">300m</button>
-				<button name="radius" class="radius" value="600">600m</button>
-				<button name="radius" class="radius" value="900">900m</button>
-				<button id="gameDone">게임끝</button>
-			</div>
-			<p id="memberPosition"></p>
-			<div id="map"></div>
-		</div>
-	</section>
-	<section id="restaurantWrap">
-		<h3 class="hidden">식당 정보 표시</h3>
-		<div id="restaurantInfo">
-			<div id="restaurantWindow">
-				<iframe id="restaurantUrl"></iframe>
-				<button id="open" value="open">펼치기</button>
-			</div>
-		</div>
-		<!-- 식당차트정보 시작 - 윤효심 -->
-		<input type="hidden" name="restaurantId" value="${restaurantId }"
+	<input type="hidden" name="restaurantId" value="${restaurantId }"
 		id="restaurantId">
+	<section id="indexContent">
 		<div class="storeSubInfoWrap">
 			<div class="ageAndGenderGraphArea">
 				<h3>연령별 / 성별 방문수</h3>
@@ -85,25 +55,7 @@
 				</div>
 			</div>
 		</div>
-		<!-- 식당차트정보 끝 - 윤효심 -->
-		
 	</section>
-	
-	<section id="chatWrap">
-	
-	</section>
-</section>
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
 	<!-- 포춘쿠키 -->
 			<c:if test="${not empty fortuneMessage }">
 			<div class="fortune">
